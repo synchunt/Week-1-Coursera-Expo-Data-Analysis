8a9594f941340d367e69150fb768d7e71ebd1e7b
Pranjal M <justmishra@outlook.com>
2020-05-26 02:22:44 +0530

    Add plot 2 bot R file and PNG

---
+++ Plot2.R
@@ -0,0 +1,13 @@
+library(lubridate)
+
+# reading relevant data
+data <- read.table("household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2880)
+
+# clearing name row and unrelevant variables, casting to right classes
+power <- power %>% mutate(V1 = as.POSIXct(dmy_hms(as.character(paste(V1, V2)))),
+                        V3 = as.numeric(as.character(V3))) %>% select(V1,V3)
+
+# create plot
+png("Plot2.png", width = 480, height = 480)
+with(power, plot(V1,V3, type="l", xlab = "", ylab = "Global Active Power (kilowatts)"))
+dev.off()
